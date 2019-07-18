760d82c3f5a9f14906f31998aa4fcdfc06198c9b
Alberto Viezzi <aviezzi@gmail.com>
2019-07-18 13:58:36 +0200

    Implemented lights in club-dashboard

---
+++ src/AppiSimo.Client/Pages/ClubSettings/Lights/LightsComponent.cs
@@ -0,0 +1,31 @@
+namespace AppiSimo.Client.Pages.ClubSettings.Lights
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Abstract;
+    using Microsoft.AspNetCore.Components;
+    using Model;
+
+    public class LightsComponent : ComponentBase
+    {
+        [Inject]
+        IUriHelper UriHelper { get; set; }
+
+        [Inject]
+        IResourceService<Light> LightService { get; set; }
+
+        protected ICollection<Light> Lights { get; private set; }
+
+        protected override async Task OnInitAsync()
+        {
+            Lights = await LightService.GetAsync();
+        }
+
+        protected void GoToEdit(Guid key) =>
+            UriHelper.NavigateTo($"/club-dashboard/lights/edit/{key}");
+
+        protected void GoToCreate() =>
+            UriHelper.NavigateTo("/club-dashboard/lights/create");
+    }
+}
