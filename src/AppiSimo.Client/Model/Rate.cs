02f0c70ef0ecaf106f59181b7e3d82a002603c4f
Alberto Viezzi <aviezzi@gmail.com>
2019-08-11 15:28:37 +0200

    Add name to rates flow

--- src/AppiSimo.Client/Model/Rate.cs
+++ src/AppiSimo.Client/Model/Rate.cs
@@ -5,6 +5,7 @@ namespace AppiSimo.Client.Model
 
     public class Rate : Entity
     {
+        public string Name { get; set; } = string.Empty;
         public LocalDate Start { get; set; }
         public LocalDate End { get; set; }
         public ICollection<DailyRate> DailyRates { get; set; } = new List<DailyRate>();
--- src/AppiSimo.Client/Pages/ClubDashboard/Details/RateDetail/RateViewModel.cs
+++ src/AppiSimo.Client/Pages/ClubDashboard/Details/RateDetail/RateViewModel.cs
@@ -1,33 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AppiSimo.Client.Model;
+using NodaTime;
+
 namespace AppiSimo.Client.Pages.ClubDashboard.Details.RateDetail
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.Linq;
-    using Model;
-    using NodaTime;
-
     public class RateViewModel : IDetailViewModel<Rate>
     {
         public Rate Entity { get; }
+
+        public bool IsNew => Entity.Id == Guid.Empty;
 
         public RateViewModel()
             : this(new Rate())
         {
         }
-        
+
         public RateViewModel(Rate rate) => Entity = rate ?? throw new NullReferenceException("Rate cannot be null.");
 
-        public bool IsNew => Entity.Id == Guid.Empty;
+        [Required(ErrorMessage = "È obbligatorio inserire un nom eper questa tariffa.")]
+        public string Name
+        {
+            get => Entity.Name;
+            set => Entity.Name = value;
+        }
+
 
         [Required(ErrorMessage = "È obbligatorio inserire la data di inizio validita'.")]
-        public LocalDate StartDate { get => Entity.Start; set => Entity.Start = value; }
+        public LocalDate StartDate
+        {
+            get => Entity.Start;
+            set => Entity.Start = value;
+        }
 
         [Required(ErrorMessage = "È obbligatorio inserire la data di fine validita'.")]
-        public LocalDate EndDate { get => Entity.End; set => Entity.End = value; }
+        public LocalDate EndDate
+        {
+            get => Entity.End;
+            set => Entity.End = value;
+        }
 
         public IEnumerable<DailyRateViewModel> DailyRatesViewModel => Entity.DailyRates.Select(dr => new DailyRateViewModel(dr));
-
+        
         public void AddDailyRate() => Entity.DailyRates.Add(new DailyRate());
 
         public void RemoveDailyRate(Guid id) => Entity.DailyRates = Entity.DailyRates.Where(dailyRate => dailyRate.Id != id).ToList();
