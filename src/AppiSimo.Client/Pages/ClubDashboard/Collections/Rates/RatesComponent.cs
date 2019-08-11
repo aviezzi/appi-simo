541ce860693420e43f19fafd9afe530a4645fa98
Alberto Viezzi <aviezzi@gmail.com>
2019-08-11 13:01:58 +0200

    Encapsulate LocalTimeSteps

--- src/AppiSimo.Client/Pages/ClubDashboard/Collections/Rates/RatesComponent.cs
+++ src/AppiSimo.Client/Pages/ClubDashboard/Collections/Rates/RatesComponent.cs
@@ -1,22 +1,21 @@
+using System;
+using System.Collections.Generic;
+using AppiSimo.Client.Abstract;
+using AppiSimo.Client.Model;
+using Microsoft.AspNetCore.Components;
+
 namespace AppiSimo.Client.Pages.ClubDashboard.Collections.Rates
 {
-    using System;
-    using System.Collections.Generic;
-    using Microsoft.AspNetCore.Components;
-    using Model;
-
     public class RatesComponent : ComponentBase
     {
-        [Inject]
-        IUriHelper UriHelper { get; set; }
+        [Inject] IUriHelper UriHelper { get; set; }
 
-        [Parameter]
-        protected ICollection<Rate> Rates { get; set; }
-        
-        protected void GoToEdit(Guid key) =>
-            UriHelper.NavigateTo($"/club-dashboard/rates/edit/{key}");
+        [Inject] protected IConverters Converters { get; set; }
 
-        protected void GoToCreate() =>
-            UriHelper.NavigateTo("/club-dashboard/rates/create");
+        [Parameter] protected ICollection<Rate> Rates { get; set; }
+
+        protected void GoToEdit(Guid key) => UriHelper.NavigateTo($"/club-dashboard/rates/edit/{key}");
+
+        protected void GoToCreate() => UriHelper.NavigateTo("/club-dashboard/rates/create");
     }
 }
