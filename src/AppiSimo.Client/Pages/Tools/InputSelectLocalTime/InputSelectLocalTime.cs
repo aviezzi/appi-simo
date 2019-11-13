9c46f3efe022206c7e8c0950aa52c7f098b52a95
Alberto Viezzi <aviezzi@outlook.com>
2019-11-13 13:21:14 +0100

    Use AuthorizeView to show/hide the sidebar menu.

--- src/AppiSimo.Client/Pages/Tools/InputSelectLocalTime/InputSelectLocalTime.cs
+++ src/AppiSimo.Client/Pages/Tools/InputSelectLocalTime/InputSelectLocalTime.cs
@@ -14,7 +14,7 @@ namespace AppiSimo.Client.Pages.Tools.InputSelectLocalTime
 
         [Parameter] public IEnumerable<LocalTime> Items { get; set; }
 
-        [Parameter] public LocalTime Value { get; set; }
+        [Parameter] public LocalTime? Value { get; set; }
 
         [Parameter] public EventCallback<LocalTime> ValueChanged { get; set; }
 
@@ -22,12 +22,24 @@ namespace AppiSimo.Client.Pages.Tools.InputSelectLocalTime
 
         protected string Time
         {
-            get => Converter.FormatValueAsString(Value);
+            get
+            {
+                if (Value == null) return string.Empty;
+
+                return Converter.FormatValueAsString(Value.Value);
+            }
             set
             {
+                if (value == string.Empty)
+                {
+                    Value = null;
+                    return;
+                }
+
                 Converter.TryParseValueFromString(value, out var result);
                 Value = result;
-                ValueChanged.InvokeAsync(Value);
+
+                ValueChanged.InvokeAsync(Value.Value);
                 CascadedEditContext?.NotifyValidationStateChanged();
             }
         }
